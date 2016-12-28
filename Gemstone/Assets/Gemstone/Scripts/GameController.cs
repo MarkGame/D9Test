463c218bcbd8c9a710efbbf7923571d1a0d9f0f4
delicious9 <delicious1238@126.com>
2016-12-28 22:16:55 +0800

    提交工程文件

---
+++ Gemstone/Assets/Gemstone/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public Gemstone gemstone;
+	public int rownum = 7;
+	public int colnum = 10;
+	public ArrayList gemstoneList;
+	// Use this for initialization
+	void Start () {
+		gemstoneList = new ArrayList ();
+		for(int rowIndex = 0; rowIndex < rownum; rowIndex++){
+			ArrayList temp = new ArrayList ();
+			for (int colIndex = 0; colIndex < colnum; colIndex++) {
+				Gemstone c = Instantiate (gemstone) as Gemstone;
+				c.transform.parent = this.transform;
+				temp.Add (c);
+				c.GetComponent<Gemstone> ().UpdatePosition (rowIndex, colIndex);
+			}
+			gemstoneList.Add (temp);
+		}
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Gemstone/Assets/Gemstone/Scripts/Gemstone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gemstone : MonoBehaviour {
+	public float xOffset = 0f;
+	public float yOffset = 0f;
+	public float interval = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdatePosition(int y, int x){
+		this.transform.position = new Vector3 (x*interval + xOffset, y*interval + yOffset, 0);
+	
+	
+	}
+}
